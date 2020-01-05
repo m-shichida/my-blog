adbfcc18cb2c430c5a2146e1b259bdc9be164c53
shichida <m.shichida@trecon.co.jp>
2020-01-06 00:18:52 +0900

    #36 トップバナーのデザイン修正

--- src/components/TopBanner/index.js
+++ src/components/TopBanner/index.js
@@ -3,20 +3,24 @@ import styles from './styles';
 import Twitter from '../../images/twitter.png';
 import Github from '../../images/github.png';
 import Qitta from '../../images/qiita.png';
+import Icon from '../../images/profile_image.jpg';
 
-const { BannerContainer, MessageContainer, Title, SnsImage, Links } = styles;
+const { BannerContainer, MessageContainer, Title, SnsImage, IconImage, Links } = styles;
 
 const TopBanner = () => {
   return (
     <BannerContainer>
       <MessageContainer>
         <Title>Shicchi's Tech Blog</Title>
+        <Links>
+          <IconImage
+            src={ Icon }
+          />
+          <a href='https://github.com/m-shichida' target='_blank' rel="noopener noreferrer"><SnsImage src={ Github } /></a>
+          <a href='https://qiita.com/shicchi7913' target='_blank' rel="noopener noreferrer"><SnsImage src={ Qitta } /></a>
+          <a href='https://twitter.com/shicchi7913' target='_blank' rel="noopener noreferrer"><SnsImage src={ Twitter } /></a>
+        </Links>
       </MessageContainer>
-      <Links>
-        <a href='https://github.com/m-shichida' target='_blank' rel="noopener noreferrer"><SnsImage src={ Github } /></a>
-        <a href='https://qiita.com/shicchi7913' target='_blank' rel="noopener noreferrer"><SnsImage src={ Qitta } /></a>
-        <a href='https://twitter.com/shicchi7913' target='_blank' rel="noopener noreferrer"><SnsImage src={ Twitter } /></a>
-      </Links>
     </BannerContainer>
   )
 }
